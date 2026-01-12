b46721ebd6364c66f1cad5b0f7bbfa79c744baef
ArturYanz <arturoyanez2004@gmail.com>
2026-01-12 20:04:27 -0300

    Week 01 Exercise 3 update

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        int guess = -1;
+
+        Random randonGenerator = new Random();
+        int number = randonGenerator.Next(1, 101);
+
+        
+
+        while (guess != number)
+        {
+
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (number < guess)
+            {
+                Console.WriteLine("Lower");
+                Console.Write("What is your guess? ");
+            }
+            else if (number > guess)
+            {
+                Console.WriteLine("Higher");
+                Console.Write("What is your guess? ");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }
+
+
+
+
+
+
+
     }
 }

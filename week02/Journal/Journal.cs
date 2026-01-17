918bc9e18c9129731d10f9a07bd38f80178439d2
ArturYanz <arturoyanez2004@gmail.com>
2026-01-17 17:45:45 -0300

    Week02 Journal Program Updated (v1)

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -1,6 +1,7 @@
 using System.IO;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 
 
@@ -24,12 +25,34 @@ public class Journal
 
     public void SaveToFile(string file)
     {
-
+        
+        using (StreamWriter outputFile = new StreamWriter(file))
+        {
+            foreach (Entry entry in _entries)
+            {
+                outputFile.WriteLine($"{entry._date}|{entry._promptText}|{entry._entryText}");
+                
+            }
+        }
     }
 
     public void LoadFromFile(string file)
     {
-        
+        string[] _lines = System.IO.File.ReadAllLines(file);
+
+        _entries.Clear();
+
+        foreach (string line in _lines)
+        {
+            string[] _parts = line.Split("|");
+
+            Entry _newEntry = new Entry();
+            _newEntry._date = _parts[0];
+            _newEntry._promptText = _parts[1];
+            _newEntry._entryText = _parts[2];
+
+            _entries.Add(_newEntry);
+        }
     }
 
 }
--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -2,24 +2,47 @@ using System;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
 
+// Hi Im Arturo Yánez, this is my project :D
+// I added some extra features:
+// 1. Input validation using int.TryParse to prevent crashes
+// 2. Validation to ensure the menu option is between 1 and 5
+// 3. Informative messages when saving, loading or quitting the program
+
 class Program
 {
     static void Main(string[] args)
     {
         Journal newjournal = new Journal();
-        
-        int _toDo = -1;
 
-        while (_toDo != 3)
+        int toDo = -1;
+        Console.WriteLine("------------Dear Diary (1.1)------------");
+        while (toDo != 5)
         {
-            Console.WriteLine("1. Write new");
-            Console.WriteLine("2. Display all");
-            Console.WriteLine("3. Quit");
-            Console.Write("What do you want to do? ");
-            _toDo = int.Parse(Console.ReadLine());
+            Console.WriteLine("     1. Write new");
+            Console.WriteLine("     2. Display all");
+            Console.WriteLine("     3. Save file");
+            Console.WriteLine("     4. Load file");
+            Console.WriteLine("     5. Quit");
+            Console.Write("        What do you want to do? ");
+            string input = Console.ReadLine();
+
+
+            bool inputValid = int.TryParse(input, out toDo);
+            if (!inputValid)
+            {
+                Console.WriteLine("Please enter a valid number");
+                Console.WriteLine("");
+                continue;
+            }
+            else if (toDo >= 6 || toDo < 1)
+            {
+                Console.WriteLine("Enter a number from 1 to 5 please...");
+                Console.WriteLine("");
+                continue;
+            }
             Console.WriteLine("");
 
-            if (_toDo == 1)
+            if (toDo == 1)
             {
                 Entry newEntry = new Entry();
                 DateTime theCurrentTime = DateTime.Now;
@@ -31,43 +54,47 @@ class Program
 
                 newEntry._promptText = prompt;
 
-                Console.Write($"{newEntry._promptText}");
+                Console.Write($"{newEntry._promptText} ");
                 newEntry._entryText = Console.ReadLine();
 
                 newjournal.AddEntry(newEntry);
                 Console.WriteLine("");
 
             }
-            else if (_toDo == 2)
+            else if (toDo == 2)
             {
                 newjournal.DisplayAll();
-                
+
             }
-            else if (_toDo >=4)
+            else if (toDo == 3)
             {
-                Console.WriteLine("Wrong button. Try Again!");
-                Console.WriteLine("");
+                Console.Write("Give a name to the file: ");
+                string file = Console.ReadLine();
+
+
+                Console.WriteLine("Saving File...");
+                newjournal.SaveToFile(file);
+
+
             }
+            else if (toDo == 4)
+            {
+                Console.Write("What is the name of the file you want to load? ");
+                string file = Console.ReadLine();
+
+                Console.WriteLine("Loading File...");
+                Console.WriteLine("");
+                newjournal.LoadFromFile(file);
+            }
+
+
+
+            
         }
-            
-            
-
-        // Entry entry1 = new Entry();
-        // DateTime theCurrentTime = DateTime.Now;
-        // string dateText = theCurrentTime.ToShortDateString();
-        // entry1._date = dateText;
-
-        // PromptGenerator generator = new PromptGenerator();
-        // string prompt = generator.GetRandomPrompt();
-
-        // entry1._promptText = prompt;
-
-        // Console.Write($"{entry1._promptText}");
-        // entry1._entryText = Console.ReadLine();
 
 
-        //entry1.DisplayEntry();
-
+        Console.WriteLine("Quiting...");
+        Console.WriteLine("----------------------------------------");
 
 
     }

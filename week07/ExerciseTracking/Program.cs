00f799fb12a7f314d8f0347ee4be4ae02f49ec8b
ArturYanz <arturoyanez2004@gmail.com>
2026-02-16 19:28:41 -0300

    Week 07 Ecercise Tracking. Final Work, Thanks for all!

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,27 @@
 using System;
 
+
+// Thanks for watching ;-)
+// -Arturo 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        Running run = new Running("16 Feb 2026", 75, 5);
+        activities.Add(run);
+
+        Cycling cycle = new Cycling("20 Jan 2026", 120, 30);
+        activities.Add(cycle);
+
+        Swimming swimm = new Swimming("31 Dec 2025", 35, 28);
+        activities.Add(swimm);
+
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
